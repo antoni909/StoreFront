ee59deffc8373426a6d633ab322f6b9c53c6a84e
antoni <antoni.dev0@gmail.com>
2022-01-25 23:57:07 -0800

    adds counter component using redux

--- frontend/src/components/Main.jsx
+++ frontend/src/components/Main.jsx
@@ -1,9 +1,11 @@
 import Categories from './Categories'
+import Counter from '../components/Counter'
 
 function Main() {
   return (
     <div >
       <h1>Main Component</h1>
+      <Counter />
       <Categories />
     </div>
   );
