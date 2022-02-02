a3000227e8fd08af2980b07e42a6669a9ce5f577
antoni <antoni.dev0@gmail.com>
2022-02-02 13:34:56 -0800

    adds inc/dec items in cart logic, keeps count of total cart items

--- frontend/src/components/CartItems.jsx
+++ frontend/src/components/CartItems.jsx
@@ -1,20 +1,61 @@
+import { useSelector, useDispatch } from "react-redux";
+import { addToCartTotals, selectCart, updateProductTotals } from "../features/cart/cartSlice"
 
-import { useSelector } from 'react-redux'
-import { selectCart } from '../features/cart/cartSlice'
+import AddIcon from '@mui/icons-material/Add';
+import IconButton from '@mui/material/IconButton';
+import RemoveIcon from '@mui/icons-material/Remove';
+import Tooltip from '@mui/material/Tooltip';
 
 export default function CartItems(){
+  const dispatch = useDispatch()
   const cart = useSelector(selectCart)
-  console.log(cart && Object.keys(cart.products))
   return(
     <>
       <h2>Your Items</h2>
       {
-        cart && Object.keys(cart.products).map((product,idx) => (
-          <ul key={idx}>
-            <li>{product}</li>
-          </ul>
+        cart && Object.keys(cart.products).map((prod) => (
+          <>
+            <div key={prod._id}>
+              <span>
+                <>{prod}: </> 
+                <Tooltip title="remove">
+                <IconButton
+                  onClick={()=>{
+                    const item = {num: -1, prod}
+                    dispatch(addToCartTotals(item.num))
+                    dispatch(updateProductTotals(item))
+                  }}
+                >
+                  <RemoveIcon 
+                    color="primary"
+                    fontSize="small"
+
+                  />
+                </IconButton>
+                </Tooltip>
+  
+                  {cart && cart.productTotals[prod]}
+
+                <Tooltip title="add">
+                  <IconButton
+                    onClick={()=>{
+                      const item = {num : 1, prod}
+                      dispatch(addToCartTotals(item.num))
+                      dispatch(updateProductTotals(item))
+                    }}
+                  >
+                    <AddIcon 
+                      color="primary"
+                      fontSize="small"
+                    
+                    />
+                  </IconButton>
+                </Tooltip>
+              </span>
+            </div>
+            <br />
+          </>
         ))
-      
       }
     </>
   )
