5002f7cd833e80eee720dd7052e75f4ebc0e8778
antoni <antoni.dev0@gmail.com>
2022-02-16 10:48:20 -0800

    adds service dir and service RESTful methods

---
+++ frontend/src/services/inventory.js
@@ -0,0 +1,37 @@
+import axios from 'axios'
+
+const service = {
+getAllCategories : async () => {
+  try{
+    const categories = await axios.get('/categories')
+    return categories
+  }catch(e){
+    console.log('ERR getAllCategories',e)
+  }
+},
+getCategory : () => {},
+updateCategoryById : () => {},
+deleteCategory : () => {},
+getAllProducts : () => {},
+getProductById : () => {},
+getProductCatId : () => {},
+updateProductById: () => {},
+deleteProductById : () => {},
+postCategory : () => {},
+postProduct : () => {},
+}
+
+export default service
+/*
+| READ ALL    |      GET      | /categories       |
+| READ ONE    |      GET      | /categories/:id   |
+| CREATE ONE  |      POST     | /categories       |
+| UPDATE ONE  |      PUT      | /categories/:id   |
+| DELETE ONE  |      DEL      | /categories/:Id   |
+| READ ALL    |      GET      | /products         |
+| READ ONE    |      GET      | /products/:id     |
+| READ ONE    |      GET      | /products/:catId  |
+| CREATE ONE  |      POST     | /products/        |
+| UPDATE ONE  |      PUT      | /products/:id     |
+| DELETE ONE  |      DEL      | /products/:id     |
+*/
