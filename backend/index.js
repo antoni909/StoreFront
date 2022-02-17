714119c366f7adf33a10a4683cc6a363b5f143e8
antoni <antoni.dev0@gmail.com>
2022-02-17 14:33:51 -0800

    fix CORS error when serving data to react client, adds new env variable for allowed client url

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,5 @@
 const { db_authed } = require('./db/models/index')
-const { start, app } = require('./src/server')
-const cors = require('cors');
-const corsOptions = {
-    origin:'http://localhost:3000', 
-    credentials:true,
-    optionSuccessStatus:200
-}
-app.use(cors(corsOptions));
+const { start } = require('./src/server')
 const PORT = process.env.PORT || 3001
 
 db_authed() && start(PORT)
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,8 +1,15 @@
 const express = require('express')
 const app = express()
+const cors = require('cors');
 const notFoundHandler = require('./handlers/404')
 const serverErrorHandler = require('./handlers/404')
 const routes = require('./routes/router')
+const corsOptions = {
+  origin:process.env.CORS_ORIGIN, 
+  credentials:true,
+  optionSuccessStatus:200
+}
+app.use(cors(corsOptions));
 app.use(express.json())
 
 app.use(routes)
